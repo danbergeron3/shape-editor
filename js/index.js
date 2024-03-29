8622486202befeee875f91fc10d2a8900967eed3
Daniel Bergeron <104526289+danbergeron3@users.noreply.github.com>
2024-03-28 20:06:11 -0400

    added basic drawing

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+let currentMode;
+let mousePressed = false; 
+
+const editorModes = {
+    pan: 'pan',
+    draw: 'draw'
+}
 
 const initCanvas = (id) => {
     return new fabric.Canvas('canvas', {
@@ -7,30 +14,47 @@ const initCanvas = (id) => {
     });
 }
 
-window.onload = (event) => {
-    const canvas = initCanvas('canvas');
-    fabric.Image.fromURL('https://wallpapers.com/images/high/red-raspberries-and-daisy-flower-roldn198zsn76ez3.jpg', (img) => {
-        canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
-            // Options to control the background image
-            scaleX: canvas.width / img.width,
-            scaleY: canvas.height / img.height
-        });
+const canvas = initCanvas('canvas');
+fabric.Image.fromURL('https://wallpapers.com/images/high/red-raspberries-and-daisy-flower-roldn198zsn76ez3.jpg', (img) => {
+    canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
+        // Options to control the background image
+        scaleX: canvas.width / img.width,
+        scaleY: canvas.height / img.height
     });
-    console.log("Canvas initialized:", canvas);
+});
 
-    // Now, canvas.renderAll() is not strictly necessary since the 
-    // canvas is automatically rendered upon creation and any updates.
-    // mouse over 
-    // panning 
+const toggleMode = (mode) => {
+    if(mode === editorModes.pan) {
+        if(currentMode === editorModes.pan) {
+            currentMode = ' ';
+        } else {
+            currentMode = editorModes.pan;
 
-    let currentMode = 'pan';
-    let mousePressed = false; 
+            // turn off draw mode 
+            canvas.isDrawingMode = false;
+            canvas.renderAll();
+        }
+    } else if (mode === editorModes.draw) {
+        if(currentMode === editorModes.draw) {
+            currentMode = '';
 
-    const mode = {
-        pan: 'pan',
+            // turn off draw when toggle draw is selected
+            canvas.isDrawingMode = false;
+            canvas.renderAll();
+        } else {
+            canvas.freeDrawingBrush = new fabric.CircleBrush(canvas);
+            canvas.freeDrawingBrush.color = 'red';
+            canvas.freeDrawingBrush.width = 15;
+            // toggle draw on and intiate drawing
+            currentMode = editorModes.draw;
+            canvas.isDrawingMode = true;
+            canvas.renderAll();
+        }
     }
+    console.log(mode);
+};
 
-    
+const setPanEvents = (canvas) => {
     canvas.on('mouse:move', (event) => {
         if(mousePressed && currentMode === 'pan') {
             canvas.setCursor('grab');
@@ -39,6 +63,9 @@ window.onload = (event) => {
             const moveEvent = event.e;
             const delta = new fabric.Point(moveEvent.movementX, moveEvent.movementY);
             canvas.relativePan(delta);
+        } else if (mousePressed && currentMode === editorModes.draw) {
+            canvas.isDrawingMode = true;
+            canvas.renderAll();
         }
     });
 
@@ -59,7 +86,16 @@ window.onload = (event) => {
             canvas.renderAll();
         }
     });
+};
 
     
-};
+console.log("Canvas initialized:", canvas);
+
+// Now, canvas.renderAll() is not strictly necessary since the 
+// canvas is automatically rendered upon creation and any updates.
+// mouse over 
+// panning 
+setPanEvents(canvas);
+    
+
 
