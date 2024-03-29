bc2cb4cac3b15fc7f4b482b0a011deefa2816b6c
Daniel Bergeron <104526289+danbergeron3@users.noreply.github.com>
2024-03-28 22:38:23 -0400

    more shapes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 let currentMode;
+let currentColor = '#000000'; 
+                                 
 let mousePressed = false; 
 
 const editorModes = {
@@ -8,11 +10,110 @@ const editorModes = {
 
 const initCanvas = (id) => {
     return new fabric.Canvas('canvas', {
-        width: 500,
-        height: 500,
+        width: 1000,
+        height: 600,
         backgroundColor: 'grey'
     });
 }
+
+const clearCanvas = (canvas) => {
+    canvas.getObjects().forEach((element) => {
+        if(element !== canvas.backgroundImage) {
+            canvas.remove(element);
+        }
+    });
+};
+
+const createRect = (canvas) => {
+    console.log("rect");
+    const canvCenter = canvas.getCenter();
+    const rect = new fabric.Rect({
+        width: 200,
+        height: 100,
+        fill: currentColor,
+        left: canvCenter.left,
+        top: canvCenter.top,
+        originX: 'center',
+        originY: 'center',
+        cornerColor: '#007FFF',
+    });
+    canvas.add(rect);
+    canvas.renderAll();
+}
+
+const createCirc = (canvas) => {
+    console.log("circ");
+    const canvCenter = canvas.getCenter();
+    const circ = new fabric.Circle({
+        radius: 100,
+        fill: currentColor,
+        left: canvCenter.left,
+        top: canvCenter.top,
+        originX: 'center',
+        originY: 'center',
+        cornerColor: '#007FFF',
+    });
+    canvas.add(circ);
+    canvas.renderAll();
+}
+
+const createLine = (canvas) => {
+    console.log("line");
+    const canvCenter = canvas.getCenter();
+    const line = new fabric.Line([50, 100, 200, 200],{
+        fill:  currentColor,
+        stroke: currentColor,
+        strokeWidth: 2,
+        left: canvCenter.left,
+        top: canvCenter.top,
+        originX: 'center',
+        originY: 'center',
+        cornerColor: '#007FFF',
+    });
+    canvas.add(line);
+    canvas.renderAll();
+}
+
+const createPoly = (canvas) => {
+    console.log("line");
+    const canvCenter = canvas.getCenter();
+    const line = new fabric.Polygon([ 
+        { x: 200, y: 10 }, 
+        { x: 250, y: 50 }, 
+        { x: 250, y: 180}, 
+        { x: 150, y: 180}, 
+        { x: 150, y: 50 }],{
+        fill:  currentColor,
+        left: canvCenter.left,
+        top: canvCenter.top,
+        originX: 'center',
+        originY: 'center',
+        cornerColor: '#007FFF',
+    });
+    canvas.add(line);
+    canvas.renderAll();
+}
+
+
+const createSquare = (canvas) => {
+    console.log("square");
+    const canvCenter = canvas.getCenter();
+    const square = new fabric.Rect({
+        width: 100,
+        height: 100,
+        fill: currentColor,
+        left: canvCenter.left,
+        top: canvCenter.top,
+        originX: 'center',
+        originY: 'center',
+        cornerColor: '#007FFF',
+
+    });
+    canvas.add(square);
+    canvas.renderAll();
+}
+
+
 
 const canvas = initCanvas('canvas');
 fabric.Image.fromURL('https://wallpapers.com/images/high/red-raspberries-and-daisy-flower-roldn198zsn76ez3.jpg', (img) => {
@@ -42,9 +143,10 @@ const toggleMode = (mode) => {
             canvas.isDrawingMode = false;
             canvas.renderAll();
         } else {
-            canvas.freeDrawingBrush = new fabric.CircleBrush(canvas);
-            canvas.freeDrawingBrush.color = 'red';
-            canvas.freeDrawingBrush.width = 15;
+            // canvas.freeDrawingBrush = new fabric.CircleBrush(canvas);
+            canvas.freeDrawingBrush.color = currentColor;
+            console.log(currentColor);
+            // canvas.freeDrawingBrush.width = 15;
             // toggle draw on and intiate drawing
             currentMode = editorModes.draw;
             canvas.isDrawingMode = true;
@@ -87,6 +189,14 @@ const setPanEvents = (canvas) => {
         }
     });
 };
+
+
+document.getElementById('colorPicker').addEventListener('change', (event) => {
+    currentColor = event.target.value;
+    canvas.freeDrawingBrush.color = currentColor;
+    console.log("set color" + canvas.freeDrawingBrush.color);
+    canvas.renderAll();
+});
 
     
 console.log("Canvas initialized:", canvas);
